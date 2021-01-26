ce2d22a4256bdf159a6ad2bcd5dfdb619b35698a
Jonathan Rico <jonathan@rico.live>
2021-01-26 08:07:30 +0100

    Implement minimal Current Time Service

---
+++ include/cts.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2021 Jonathan Rico
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __CTS_H_
+#define __CTS_H_
+
+#include <stdbool.h>
+#include <zephyr/types.h>
+
+struct date_time {
+	uint16_t year;
+	uint8_t month;
+	uint8_t day;
+	uint8_t hours;
+	uint8_t minutes;
+	uint8_t seconds;
+};
+
+typedef enum {
+	UNKNOWN = 0,
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY,
+} day_of_week_t;
+
+struct day_of_week {
+	uint8_t day_of_week;
+};
+
+struct day_date_time {
+	struct date_time date_time;
+	struct day_of_week day_of_week;
+};
+
+struct exact_time_256 {
+	struct day_date_time day_date_time;
+	uint8_t fractions256; /* Number of 1/256 fractions of a second */
+};
+
+typedef enum {
+	MANUAL_TIME_UPDATE = 0,
+	EXTERNAL_REFERENCE_TIME_UPDATE,
+	CHANGE_OF_TIME_ZONE,
+	CHANGE_OF_DST,
+} adjust_reason_t;
+
+struct current_time {
+	struct exact_time_256 exact_time_256;
+	uint8_t adjust_reason;
+};
+
+int bt_cts_set_current_time(struct current_time *p_current_time);
+struct current_time *bt_cts_get_current_time(void);
+
+#endif // __CTS_H_
---
+++ src/cts.c
@@ -0,0 +1,109 @@
+/** @file
+ *  @brief GATT Current Time Service
+ */
+
+/*
+ * Copyright (c) 2021 Jonathan Rico
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <init.h>
+#include <stdbool.h>
+#include <zephyr/types.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/gatt.h>
+#include <bluetooth/uuid.h>
+
+#include "cts.h"
+#include "clock.h"
+
+#define LOG_LEVEL CONFIG_BT_CTS_LOG_LEVEL
+#include <logging/log.h>
+LOG_MODULE_REGISTER(cts);
+
+static struct current_time current_local_time;
+
+static void cts_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value)
+{
+	ARG_UNUSED(attr);
+
+	bool notif_enabled = (value == BT_GATT_CCC_NOTIFY);
+
+	LOG_INF("BAS Notifications %s", notif_enabled ? "enabled" : "disabled");
+}
+
+static ssize_t read_current_time(struct bt_conn *conn,
+				 const struct bt_gatt_attr *attr, void *buf,
+				 uint16_t len, uint16_t offset)
+{
+	LOG_INF("Read attribute");
+	/* Read values from clock */
+	bt_cts_get_current_time();
+	return bt_gatt_attr_read(conn, attr, buf, len, offset,
+				 &current_local_time,
+				 sizeof(current_local_time));
+}
+
+static ssize_t write_current_time(struct bt_conn *conn,
+				  const struct bt_gatt_attr *attr,
+				  const void *buf, uint16_t len,
+				  uint16_t offset, uint8_t flags)
+{
+	LOG_INF("Write attribute");
+
+	/* TODO: check data read */
+	memcpy(&current_local_time, buf, len);
+
+	return 0;
+}
+
+BT_GATT_SERVICE_DEFINE(
+	cts, BT_GATT_PRIMARY_SERVICE(BT_UUID_CTS),
+	BT_GATT_CHARACTERISTIC(BT_UUID_CTS_CURRENT_TIME,
+			       BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY |
+				       BT_GATT_CHRC_WRITE,
+			       BT_GATT_PERM_READ | BT_GATT_CHRC_WRITE,
+			       read_current_time, write_current_time,
+			       &current_local_time),
+	BT_GATT_CCC(cts_ccc_cfg_changed,
+		    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE), );
+
+/* TODO: change below code and implement current time service */
+
+static int cts_init(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	memset(&current_local_time, 0, sizeof(current_local_time));
+
+	return 0;
+}
+
+struct current_time *bt_cts_get_current_time(void)
+{
+	time_struct_t * p_time = clock_get_time_p();
+	current_local_time.exact_time_256.day_date_time.date_time.hours = p_time->hours;
+	current_local_time.exact_time_256.day_date_time.date_time.minutes = p_time->minutes;
+	current_local_time.exact_time_256.day_date_time.date_time.seconds = p_time->seconds;
+
+	return &current_local_time;
+}
+
+int bt_cts_set_current_time(struct current_time *p_current_time)
+{
+	int rc;
+
+	/* TODO: check the passed values before blind write */
+	memcpy(&current_local_time, p_current_time, sizeof(current_local_time));
+
+	rc = bt_gatt_notify(NULL, &cts.attrs[1], &current_local_time,
+			    sizeof(current_local_time));
+
+	return rc == -ENOTCONN ? 0 : rc;
+}
+
+SYS_INIT(cts_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
