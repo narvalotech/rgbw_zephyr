ac9f90fb0e9d018f6256fb4fbc90825037bb8ca0
Jonathan Rico <jonathan@rico.live>
2021-02-02 20:40:47 +0100

    ble: add encryption support

--- src/ble.c
+++ src/ble.c
@@ -38,6 +38,10 @@ static void connected(struct bt_conn *conn, uint8_t err)
 	display_clear();
 	display_string("connect", 0, 50);
 	board_enable_5v(0);
+
+	if (bt_conn_set_security(conn, BT_SECURITY_L4)) {
+		printk("Failed to set security\n");
+	}
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
@@ -50,9 +54,43 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 	board_enable_5v(0);
 }
 
+static void identity_resolved(struct bt_conn *conn, const bt_addr_le_t *rpa,
+			      const bt_addr_le_t *identity)
+{
+	char addr_identity[BT_ADDR_LE_STR_LEN];
+	char addr_rpa[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(identity, addr_identity, sizeof(addr_identity));
+	bt_addr_le_to_str(rpa, addr_rpa, sizeof(addr_rpa));
+
+	printk("Identity resolved %s -> %s\n", addr_rpa, addr_identity);
+}
+
+static void security_changed(struct bt_conn *conn, bt_security_t level,
+			     enum bt_security_err err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (!err) {
+		printk("Security changed: %s level %u\n", addr, level);
+		board_enable_5v(1);
+		display_clear();
+		display_string("encrypted", 0, 50);
+		board_enable_5v(0);
+
+	} else {
+		printk("Security failed: %s level %u err %d\n", addr, level,
+		       err);
+	}
+}
+
 static struct bt_conn_cb conn_callbacks = {
 	.connected        = connected,
 	.disconnected     = disconnected,
+	.identity_resolved = identity_resolved,
+	.security_changed = security_changed,
 };
 
 static struct bt_conn_auth_cb conn_auth_callbacks;
@@ -80,10 +118,57 @@ static struct bt_lbs_cb lbs_callbacs = {
 	.button_cb = app_button_cb,
 };
 
+static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Passkey for %s: %06u\n", addr, passkey);
+
+	board_enable_5v(1);
+	display_clear();
+	display_string("passkey", 0, 50);
+	uint8_t high = passkey / 10000;
+	uint8_t mid = (passkey / 100) - (high * 100);
+	uint8_t low = passkey - ((passkey / 100) * 100);
+	display_bcd(high, mid, low, 1000);
+	board_enable_5v(0);
+}
+
+static void auth_cancel(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing cancelled: %s\n", addr);
+}
+
+static void pairing_complete(struct bt_conn *conn, bool bonded)
+{
+	printk("Pairing Complete\n");
+}
+
+static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
+{
+	printk("Pairing Failed (%d). Disconnecting.\n", reason);
+	bt_conn_disconnect(conn, BT_HCI_ERR_AUTH_FAIL);
+}
+
+static struct bt_conn_auth_cb auth_cb_display = {
+	.passkey_display = auth_passkey_display,
+	.passkey_entry = NULL,
+	.cancel = auth_cancel,
+	.pairing_complete = pairing_complete,
+	.pairing_failed = pairing_failed,
+};
+
 int ble_init(void)
 {
 	int err;
 
+	bt_conn_auth_cb_register(&auth_cb_display);
 	bt_conn_cb_register(&conn_callbacks);
 	if (IS_ENABLED(CONFIG_BT_LBS_SECURITY_ENABLED)) {
 		bt_conn_auth_cb_register(&conn_auth_callbacks);
