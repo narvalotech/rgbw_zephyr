98bf12e5e29f71df7a83756405037c314e9ae59a
Jonathan Rico <jonathan@rico.live>
2021-05-09 09:14:16 +0200

    accel: Import trigger + filter from ledwatch project
    Upstream driver is for lis2dh and trigger registers are different from
    lis3dh. I don't have enough time right now to create a new one for
    lis3dh with all the correct trigger handling.
    Instead I import only the necessary bits from my previous project, I'll
    make a proper zephyr driver if I have to reuse lis3dh someday.

--- include/accel.h
+++ include/accel.h
@@ -6,4 +6,96 @@ int accel_get_mg(int32_t accel[3]);
 int accel_init(void);
 int accel_high_latency(bool high);
 
+/* Imported from ledwatch project
+ * TODO: Improve upstream driver someday */
+//----Accel sample rate: ctrl_1 ODR bits----
+#define ACC_RATE_1       1
+#define ACC_RATE_10      2
+#define ACC_RATE_25      3
+#define ACC_RATE_50      4
+#define ACC_RATE_100     5
+#define ACC_RATE_200     6
+#define ACC_RATE_400     7
+#define ACC_RATE_1250    9
+#define ACC_RATE_1600_LP 8
+#define ACC_RATE_5000_LP 9
+
+//----Accel Full-scale value: ctrl_4 FS bits----
+#define ACC_FS_2G  0
+#define ACC_FS_4G  1
+#define ACC_FS_8G  2
+#define ACC_FS_16G 3
+
+//----Interrupt Generator 1 modes----
+#define IG_OR     0
+#define IG_6D_MOV 0x40
+#define IG_AND    0x80
+#define IG_6D_POS 0xC0
+
+//----Intgen axes----
+#define I1_ZH 0x20
+#define I1_ZL 0x10
+#define I1_YH 0x08
+#define I1_YL 0x04
+#define I1_XH 0x02
+#define I1_XL 0x01
+#define I1_INT 0x40
+
+//----Tap Interrupt Axes / Modes
+#define TAP_ZD 0x20
+#define TAP_ZS 0x10
+#define TAP_YD 0x08
+#define TAP_YS 0x04
+#define TAP_XD 0x02
+#define TAP_XS 0x01
+
+//----Tap Interrupt Sources
+#define TAP_INT    0x40
+#define TAP_D      0x20
+#define TAP_S      0x10
+#define TAP_SIGN   0x08
+#define TAP_Z      0x04
+#define TAP_Y      0x02
+#define TAP_X      0x01
+
+//----I1 Interrupt Sources----
+#define I1_CLICK     0x80
+#define I1_IG1       0x40
+#define I1_IG2       0x20
+#define I1_DRDY1     0x10
+#define I1_DRDY2     0x08
+#define I1_WTM       0x04
+#define I1_OVERRUN   0x02
+
+//----I2 Interrupt Sources----
+#define I2_CLICK     0x80
+#define I2_IG1       0x40
+#define I2_BOOT      0x10
+#define I2_HLACTIVE  0x02
+
+//----High-pass Filter Configuration----
+#define HPF_0   0x10
+#define HPF_1   0x20
+#define HPM_0   0x40
+#define HPM_1   0x80
+#define HPDATA  0x08
+#define HPCLICK 0x04
+#define HPINT2  0x02
+#define HPINT1  0x01
+
+#define HP_MODE_NORMAL_RESET 0x00
+#define HP_MODE_REF          0x40
+#define HP_MODE_NORMAL       0x80
+#define HP_MODE_AUTORESET    0xC0
+
+//----Navigation defines----
+#define LEFT   1
+#define RIGHT  2
+#define UP     3
+#define DOWN   4
+#define CLICK  5
+#define DCLICK 6
+
+
+
 #endif
--- src/accel.c
+++ src/accel.c
@@ -2,10 +2,19 @@
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/sensor.h>
+#include <drivers/gpio.h>
+#include <drivers/i2c.h>
+#include "state.h"
 #include "disp.h"
 #include "accel.h"
 
-static const struct device *sensor;
+static const struct device* sensor;
+static const struct device* sensor_bus;
+#define SENSOR_ADDR (uint8_t)DT_PROP(DT_NODELABEL(accel), reg)
+
+extern struct g_state state;
+
+void acc_app_init(void);
 
 int accel_get_mg(int32_t accel[3])
 {
@@ -45,6 +54,7 @@ int accel_init(void)
 	 * initialized at kernel boot time.
 	 */
 	sensor = device_get_binding(DT_LABEL(DT_NODELABEL(accel)));
+	sensor_bus = device_get_binding(DT_LABEL(DT_BUS(DT_NODELABEL(accel))));
 
 	if (sensor == NULL) {
 		printk("Could not get %s device\n",
@@ -52,5 +62,195 @@ int accel_init(void)
 		return -1;
 	}
 
+	acc_app_init();
+
 	return 0;
+}
+
+/* Imported from ledwatch project
+ * TODO: Improve upstream driver someday */
+static uint8_t acc_read_reg(uint8_t reg_addr)
+{
+	uint8_t value = 0;
+
+	i2c_reg_read_byte(sensor_bus,
+			  SENSOR_ADDR,
+			  reg_addr, &value);
+
+	return value;
+}
+
+static void acc_write_reg(uint8_t reg_addr, uint8_t value)
+{
+	i2c_reg_write_byte(sensor_bus,
+			   SENSOR_ADDR,
+			   reg_addr, value);
+}
+
+static void acc_hpf_config(uint8_t config)
+{
+	acc_write_reg(0x21, config); // Write ctrl_reg2
+}
+
+static void acc_int1_sources(uint8_t sources)
+{
+	sources &= 0xFF; // 8 bits
+
+	acc_write_reg(0x22, sources); // Write register
+}
+
+static void acc_int2_sources(uint8_t sources)
+{
+	sources &= 0xFF; // 8 bits
+
+	acc_write_reg(0x25, sources); // Write register
+}
+
+static void acc_click_set(uint8_t sources, uint16_t threshold,
+			  uint16_t limit_ms, uint16_t latency_ms,
+			  uint16_t window_ms)
+{
+	//----Config----
+	uint8_t oldcfg = acc_read_reg(0x38);
+	sources &= 0x3F; // 6 bits
+	sources |= oldcfg;
+	acc_write_reg(0x38, sources); // Write tap_cfg
+
+
+	//----Threshold----
+	uint8_t fs = acc_read_reg(0x23);
+	fs >>= 4;   // Data in MSB
+	fs &= 0x03; // 2 bits
+
+	switch(fs) {
+		case ACC_FS_2G:
+			threshold /= 16;
+			break;
+		case ACC_FS_4G:
+		threshold /= 31;
+			break;
+		case ACC_FS_8G:
+			threshold /= 63;
+			break;
+		case ACC_FS_16G:
+			threshold /= 125;
+			break;
+		default:
+			return;
+	}
+
+	threshold &= 0x7F; // 7 bits
+	acc_write_reg(0x3A, threshold); // Write tap_thr
+
+
+	//----Time parameters----
+	float factor_ms;
+	uint8_t rate = acc_read_reg(0x20);  // Read cfg_reg_1
+	rate >>= 4;   // Data is in MSB
+	rate &= 0x0F; // 4 bits
+
+	switch(rate) {
+		case ACC_RATE_1:
+			factor_ms = 1000;
+			break;
+		case ACC_RATE_10:
+			factor_ms = 100;
+			break;
+		case ACC_RATE_25:
+			factor_ms = 40;
+			break;
+		case ACC_RATE_50:
+			factor_ms = 20;
+			break;
+		case ACC_RATE_100:
+			factor_ms = 10;
+			break;
+		case ACC_RATE_200:
+			factor_ms = 5;
+			break;
+		case ACC_RATE_400:
+			factor_ms = 2.5;
+			break;
+		case ACC_RATE_1250:
+			factor_ms = 0.8;
+			break;
+		case ACC_RATE_1600_LP:
+			factor_ms = 0.625;
+			break;
+		// case ACC_RATE_5000_LP:
+		// 	factor_ms = 5;
+		// 	break;
+		default:
+			return;
+			break;
+	}
+	limit_ms /= factor_ms;
+	latency_ms /= factor_ms;
+	window_ms /= factor_ms;
+
+	limit_ms &= 0x7F; // 7 bits
+	latency_ms &= 0xFF; // 8 bits
+	window_ms &= 0xFF; // 8 bits
+
+	acc_write_reg(0x3B, limit_ms); // Write tap_limit
+	acc_write_reg(0x3C, latency_ms); // Write tap_latency
+	acc_write_reg(0x3D, window_ms); // Write tap_limit
+}
+
+/* App-level */
+static void acc_enable_click(void)
+{
+	acc_click_set(TAP_ZD, // Enable Z double-tap, X and Y single-tap
+		      800, // Tap-threshold is 0.3 G
+		      200, // Tap detected if acc_value decreases within 180ms
+		      80, // Pause 80ms before starting double-tap detection
+		      100); // User has to tap again withing 100 ms to register a double-tap
+	// Enable only Z-axis double-click
+	acc_write_reg(0x38, TAP_ZD);
+}
+
+static struct gpio_callback acc_cb_data;
+static void acc_callback(const struct device *dev,
+			 struct gpio_callback *cb,
+			 uint32_t pins)
+{
+	k_wakeup(state.main_tid); /* Wake from sleep */
+}
+
+#define ACC_IRQ1_PIN     DT_GPIO_PIN(DT_NODELABEL(acc_int1), gpios)
+#define ACC_IRQ2_PIN     DT_GPIO_PIN(DT_NODELABEL(acc_int2), gpios)
+
+void acc_app_init(void)
+{
+	// Enable hpf on click only, fc = 1Hz @ fs=50Hz
+	acc_hpf_config(HP_MODE_NORMAL | HPCLICK);
+	acc_enable_click(); // Enable doubleclick detection on Z-axis
+
+	/* Configure interrupt pins */
+
+	const struct device *gpio_dev;
+	gpio_dev = device_get_binding(DT_GPIO_LABEL(DT_NODELABEL(acc_int1), gpios));
+
+	gpio_pin_interrupt_configure(gpio_dev,
+				     ACC_IRQ1_PIN,
+				     GPIO_INT_EDGE_RISING);
+	gpio_pin_interrupt_configure(gpio_dev,
+				     ACC_IRQ2_PIN,
+				     GPIO_INT_EDGE_RISING);
+	gpio_pin_configure(gpio_dev,
+			   ACC_IRQ1_PIN,
+			   DT_GPIO_FLAGS(DT_NODELABEL(acc_int1), gpios));
+	gpio_pin_configure(gpio_dev,
+			   ACC_IRQ2_PIN,
+			   DT_GPIO_FLAGS(DT_NODELABEL(acc_int2), gpios));
+
+	gpio_init_callback(&acc_cb_data,
+			   acc_callback,
+			   BIT(ACC_IRQ1_PIN) | BIT(ACC_IRQ2_PIN));
+	gpio_add_callback(gpio_dev, &acc_cb_data);
+
+	// Tap-detection interrupt on output 1
+	acc_int1_sources(I1_CLICK);
+	// Orientation detection interrupt on output 2
+	acc_int2_sources(I2_CLICK | I2_IG1 | I2_HLACTIVE);
 }
